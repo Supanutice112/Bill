3631aa1d56d59ae033f1757f59926c6c8e424305
Supanut <Supanut_m@cmu.ac.th>
2024-11-21 09:56:02 +0700

    First commit

---
+++ src/app/Input/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from "../components/Nav"
+const page = () => {
+  return (
+    <div>
+            <h1>Input page</h1>
+            <Nav/>
+            </div>
+  )
+}
+
+export default page
